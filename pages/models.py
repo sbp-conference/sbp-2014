da41f41236555a46f1d7b681b097411c5cb9fe68
Kat Chuang <katychuang@gmail.com>
2013-09-30 11:18:57 -0400

    update committee page

--- pages/models.py
+++ pages/models.py
@@ -15,3 +15,16 @@ class Page(models.Model):
 
     def __unicode__(self):
         return u'%s %s %s' % (self.page_title, self.main_article, self.sidebar)
+
+
+class Committee(models.Model):
+    role        = models.CharField(max_length=50)
+    full_name   = models.CharField(max_length=200)
+    affiliation = models.TextField(blank=True)
+    link        = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ['id']
+
+    def __unicode__(self):
+        return u'%s %s %s %s' % (self.role, self.full_name, self.affiliation, self.link)
--- pages/views.py
+++ pages/views.py
@@ -12,14 +12,6 @@ def home(request, **kwargs):
 
     return render_to_response("base/index.html", context, RequestContext(request))
 
-def about(request, **kwargs):
-    page_title = kwargs.get('page')
-    stringoftext = "<div class=\"title\"><h3>Our Story</h3></div><p><i>The 2013 International Conference on Social Computing, Behavioral-Cultural Modeling, and Prediction (SBP)</i> is a multidisciplinary conference with a selective single paper track and poster session.  SBP also invites  a small number of high quality tutorials and nationally recognized keynote speakers.</p><p>The SBP conference provides a forum for researchers and practitioners from academia, industry, and government agencies to exchange ideas on current challenges in social computing, behavioral modeling and prediction, and on state-of-the-art methods and best practices being adopted to tackle these challenges. Interactive events at the conference are designed to promote cross-disciplinary contact.</p><p><b>Social Computing</b> harnesses the power of computational methods to study social behavior within a social context. <b>Behavioral Cultural  modeling</b> refers to representing behavior and culture in the abstract, and is a convenient and powerful way to conduct virtual experiments and scenario analysis. Both social computing and behavioralcultural  modeling are techniques designed to achieve a better understanding of complex behaviors, patterns, and associated outcomes of interest. Moreover, these approaches are inherently interdisciplinary; subsystems and system components exist at multiple levels of analysis (i.e., \"cells to societies\") and across multiple disciplines, from engineering and the computational sciences to the social and health sciences.</p>"
-    context = { 'page_title': page_title,
-                'page_contents': stringoftext,
-                'fonts': ''}
-    return render_to_response("base/page.html", context, RequestContext(request))
-
 def cfp(request, **kwargs):
     context = { 'page_title': 'Call for Papers',
                 'name': 'home',
@@ -27,7 +19,7 @@ def cfp(request, **kwargs):
     return render_to_response("base/cfp.html", context, RequestContext(request))
 
 def committee(request, **kwargs):
-    context = { 'page_title': 'Committee',
+    context = { 'page_title': 'Committee Members',
                 'name': 'home',
                 'fonts': ''}
     return render_to_response("base/committee.html", context, RequestContext(request))
